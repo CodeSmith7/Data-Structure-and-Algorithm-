ec46710e6408b61485269326085335dbface9d32
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2023-07-28 08:52:51 +0530

    Update kedane_algo.cpp

--- arrays/kedane_algo.cpp
+++ arrays/kedane_algo.cpp
@@ -7,15 +7,14 @@ int main()
     cout<<x<<" ";
     int sum=0,maxi=a[0],i=0;
     while(i<8)
-    {    //step 1
-        sum=sum+a[i];
-        
-        //step 2
-        maxi=max(maxi,sum);
+    {  //step 1
+         sum=sum+a[i];
+       //step 2
+         maxi=max(maxi,sum);
        //step 3
-        if(sum<0)
-        sum = 0;
+         if(sum<0)
+          sum = 0;
         i++;
     }
-    cout<<"\nMaximum of of elements of sub array is - "<<maxi;
+    cout<<"\nMaximum sum  of elements of sub array is - "<< maxi;
 }
