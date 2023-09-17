54ec72593df9791e8c1b4d46325bce42025555f4
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2023-09-17 19:25:22 +0530

    Create printf_functioning.cpp

---
+++ imp_concept/printf_functioning.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main () {
+    int x = 1987;
+    printf("%d", printf("%d", printf("%d", x)));  // gives out put as ->   198741
+
+    printf("\n") ;
+    printf("%d",printf("%d",44));  //output is -> 442
+    return(0);
+}
