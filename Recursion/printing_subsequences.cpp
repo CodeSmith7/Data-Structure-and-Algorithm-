45350201a2094216123680f9bcad03566ee3e742
Abhishek Bhadauriya <138781677+CodeSmith7@users.noreply.github.com>
2025-11-02 12:54:59 +0530

    Update printing_subsequences.cpp

--- Recursion/printing_subsequences.cpp
+++ Recursion/printing_subsequences.cpp
@@ -21,3 +21,38 @@ void solve(){
 
    printSub(0, nums, sub);
 }
+
+
+//code for printing one subsequence (whose sum  == k)
+bool printOneSub(int idx, vector<int>& nums,int s, vector <int>& sub, int sum) {
+   if ( idx == nums.size()) {
+      if ( s == sum ) {
+         for (auto it : sub ) {
+            cout << it << " ";
+         }
+         cout << endl;
+         return true;
+      } else {
+         return false;
+      }
+   }
+   s = s + nums[idx];
+
+   sub.push_back(nums[idx]);
+
+  if ( printOneSub(idx+1,nums, s, sub, sum) == true) {
+   return true;
+  } 
+
+   s= s - nums[idx];
+   sub.pop_back();
+
+   if ( printOneSub(idx+1,nums, s, sub, sum) == true) {
+      return true;
+   } 
+
+   return false;
+
+   
+}
+
